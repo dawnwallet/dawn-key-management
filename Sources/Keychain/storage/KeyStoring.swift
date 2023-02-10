a1e677063ad67683ca28eb81c30919e3e7f28391
Isaac <koideapps@gmail.com>
2023-02-10 12:02:43 -0800

    key storage implementation

--- Sources/Keychain/storage/KeyStoring.swift
+++ Sources/Keychain/storage/KeyStoring.swift
@@ -1,6 +1,38 @@
 import Foundation
 
 protocol KeyStoring {
-    func set(_ value: Data, key: String)
-    func get(_ key: String) -> Data?
+    func set(data: Data, key: String) -> OSStatus
+    func get(key: String) -> Data?
+}
+
+final class KeyStorage: KeyStoring {
+
+    func set(data: Data, key: String) -> OSStatus {
+        let query = [
+            kSecClass as String: kSecClassGenericPassword as String,
+            kSecAttrAccount as String: key,
+            kSecValueData as String: data
+        ] as [String : Any]
+
+        SecItemDelete(query as CFDictionary)
+        return SecItemAdd(query as CFDictionary, nil)
+    }
+
+    func get(key: String) -> Data? {
+        let query = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecReturnData as String: kCFBooleanTrue,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ] as [String : Any]
+
+        var dataTypeRef: AnyObject?
+        let status: OSStatus = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
+
+        if status == noErr {
+            return dataTypeRef as! Data?
+        } else {
+            return nil
+        }
+    }
 }
