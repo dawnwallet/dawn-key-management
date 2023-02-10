32bc3f4c20046fb38ea6387ed0631379ad7f6129
Isaac <koideapps@gmail.com>
2023-02-09 21:29:48 -0800

    basic package structure + readme

--- Package.swift
+++ Package.swift
@@ -17,22 +17,13 @@ let package = Package(
         .package(url: "https://github.com/GigaBitcoin/secp256k1.swift", branch: "main")
     ],
     targets: [
-        .target(
-            name: "Keychain",
-            dependencies: [
-                "CryptoSwift",
-                .product(name: "secp256k1", package: "secp256k1.swift")
-            ]),
-        .testTarget(
-            name: "KeychainTests",
-            dependencies: ["Keychain"]),
-        .target(
-            name: "Keystore",
-            dependencies: [
-                .product(name: "secp256k1", package: "secp256k1.swift")
-            ]),
-        .testTarget(
-            name: "KeystoreTests",
-            dependencies: ["Keystore"]),
+        .target(name: "Keychain", dependencies: [.product(name: "secp256k1", package: "secp256k1.swift"), "Model"]),
+        .testTarget(name: "KeychainTests", dependencies: ["Keychain"]),
+        .target(name: "Keystore", dependencies: ["Model"]),
+        .testTarget(name: "KeystoreTests", dependencies: ["Keystore"]),
+        .target(name: "Model", dependencies: ["CryptoSwift"]),
+        .testTarget(name: "ModelTests", dependencies: ["Keychain"]),
+        .target(name: "Signing", dependencies: ["Model", "Keychain"]),
+        .testTarget(name: "SigningTests", dependencies: ["Keychain"]),
     ]
 )
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # KeyManagement
 
 Keystore for managing Seeds and Private Keys & Keychain for generating Secure Enclave Keys
+
+Model
+    -- Ethereum Classes and Constants
+Keychain
+    -- Secure Enclave encryption & decryption
+Keystore
+    -- Wallet storage
+Signing
+    -- Signature methods
