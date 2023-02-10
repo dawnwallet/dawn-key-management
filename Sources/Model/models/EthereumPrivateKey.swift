c8a7e58ebd9ec43d5a0952a92f7c257b6be93d01
Isaac <koideapps@gmail.com>
2023-02-10 02:37:28 -0800

    typos

--- Sources/Model/models/EthereumPrivateKey.swift
+++ Sources/Model/models/EthereumPrivateKey.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-/// Class (reference) instead of Struct (
+/// Class (reference) instead of Struct (value) to avoid duplicated objects in memory
 public final class EthereumPrivateKey {
 
     enum Constants {
@@ -15,7 +15,7 @@ public final class EthereumPrivateKey {
     public let rawBytes: [UInt8]
 
     /// Creates a new EthereumPrivateKey with the given byte array
-    /// - Parameter rawBytes: The pruvate key bytes
+    /// - Parameter rawBytes: The private key bytes
     public init(rawBytes: [UInt8]) {
         self.rawBytes = rawBytes
         /// Closure called to return the pointer of the contiguous block of memory for the raw bytes.
@@ -25,8 +25,10 @@ public final class EthereumPrivateKey {
             mlock(pointer.baseAddress, pointer.count)
         }
     }
+
+    /// Deinit is called immediatly before a class instance is deallocated
     deinit {
-        /// Closure used to return the pointer of the contiguous storage for the raw bytes.
+        /// Closure called to return the pointer of the contiguous storage for the raw bytes.
         self.rawBytes.withUnsafeBufferPointer { (pointer) -> Void in
             /// Unlock the selected region of address space, starting from the first element of the buffer from the pointer
             /// https://man7.org/linux/man-pages/man2/mlock.2.html
