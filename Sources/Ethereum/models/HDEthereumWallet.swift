73c1e0272c40884e6476d3559cc0ad52637a3fae
Isaac <koideapps@gmail.com>
2023-02-16 13:21:23 -0800

    decrypt seed phrase

--- Sources/Ethereum/models/HDEthereumWallet.swift
+++ Sources/Ethereum/models/HDEthereumWallet.swift
@@ -8,12 +8,15 @@ import struct Model.EthereumPublicKey
 import struct Model.EthereumAddress
 import class Keychain.KeyEncryptor
 import class Keychain.KeyStorage
+import protocol Keychain.KeyDecrypting
+import class Keychain.KeyDecryptor
 
 public final class HDEthereumWallet {
 
     private let privateKey: HDEthereumPrivateKey
     private let seed: ByteArray
     private let encrypt: KeyEncryptor
+    private let decrypt: KeyDecrypting
     private let storage: KeyStorage
 
     public enum Error: Swift.Error {
@@ -24,7 +27,7 @@ public final class HDEthereumWallet {
         static let bitcoinSeed: ByteArray = [66, 105, 116, 99, 111, 105, 110, 32, 115, 101, 101, 100]
     }
 
-    /// Creates a new EthereumSeedPhrase generating a new seed phrase
+    /// Creates a new HDEthereumWallet generating a new seed phrase
     /// - Parameter seed: The seed in bytes format
     public convenience init() throws {
         let mnemonic: String = try Mnemonic.generateMnemonic(strength: 128)
@@ -32,20 +35,33 @@ public final class HDEthereumWallet {
         try self.init(seed: deterministicSeed)
     }
 
-    /// Creates a new EthereumSeedPhrase with the given mnemonic string
+    /// Creates a new HDEthereumWallet with the given mnemonic string
     /// - Parameter seed: The seed in bytes format
     public convenience init(mnemonic: String) throws {
         let deterministicSeed: ByteArray = try Mnemonic.deterministicSeedBytes(from: mnemonic)
         try self.init(seed: deterministicSeed)
     }
 
-    /// Creates a new EthereumSeedPhrase with the given seed
+    /// Creates a new HDEthereumWallet with the given id
     /// - Parameter seed: The seed in bytes format
-    public init(seed: ByteArray, encrypt: KeyEncryptor = KeyEncryptor(), storage: KeyStorage = KeyStorage()) throws {
+    public convenience init(id: String, decrypt: KeyDecrypting = KeyDecryptor(), storage: KeyStorage = KeyStorage()) throws {
+        let seed: ByteArray = try HDEthereumWallet.decryptSeedPhrase(id, storage: storage, decrypt: decrypt)
+        try self.init(seed: seed, decrypt: decrypt, storage: storage)
+    }
+
+    /// Creates a new HDEthereumWallet with the given seed
+    /// - Parameter seed: The seed in bytes format
+    public init(
+        seed: ByteArray,
+        encrypt: KeyEncryptor = KeyEncryptor(),
+        decrypt: KeyDecrypting = KeyDecryptor(),
+        storage: KeyStorage = KeyStorage()
+    ) throws {
         let hmac = HMAC(key: Constants.bitcoinSeed, variant: .sha2(.sha512))
         let computedHMAC = try hmac.authenticate(seed)
         self.seed = seed
         self.encrypt = encrypt
+        self.decrypt = decrypt
         self.storage = storage
         self.privateKey = HDEthereumPrivateKey(
             key: EthereumPrivateKey(rawBytes: ByteArray(computedHMAC[0..<32])),
@@ -82,6 +98,19 @@ public final class HDEthereumWallet {
 
 // Encryption / Decryption
 extension HDEthereumWallet {
+    @discardableResult
+    static public func decryptSeedPhrase(_ id: String, storage: KeyStorage, decrypt: KeyDecrypting) throws -> ByteArray {
+        // 1. Get the ciphertext stored in the keychain
+        guard let ciphertext = try storage.get(key: id) else {
+            throw Error.retrieveSeedBytes
+        }
+
+        // 2. Decrypt the seed
+        let seed = try decrypt.decryptSeed(id, cipherText: ciphertext)
+
+        return seed
+    }
+
     @discardableResult
     public func encryptSeedPhrase() throws -> (seed: ByteArray, id: String) {
         let seedPhraseId = UUID().uuidString
--- Sources/Keychain/encryption/KeyDecrypting.swift
+++ Sources/Keychain/encryption/KeyDecrypting.swift
@@ -1,24 +1,36 @@
 import Foundation
+import Model
 import class Model.EthereumPrivateKey
 
-protocol KeyDecrypting {
-    func decrypt(_ address: String, cipherText: Data) throws -> EthereumPrivateKey
+public protocol KeyDecrypting {
+    func decryptSeed(_ id: String, cipherText: Data) throws -> ByteArray
+    func decryptPrivateKey(_ address: String, cipherText: Data) throws -> EthereumPrivateKey
 }
 
 public final class KeyDecryptor: KeyDecrypting {
 
     public init() { }
 
-    public func decrypt(_ address: String, cipherText: Data) throws -> EthereumPrivateKey {
+    public func decryptSeed(_ id: String, cipherText: Data) throws -> ByteArray {
+        let decryptedBytes = try decrypt(id, cipherText: cipherText)
+        return decryptedBytes
+    }
+
+    public func decryptPrivateKey(_ address: String, cipherText: Data) throws -> EthereumPrivateKey {
+        let decryptedBytes = try decrypt(address, cipherText: cipherText)
+        return EthereumPrivateKey(rawBytes: decryptedBytes)
+    }
+
+    private func decrypt(_ id: String, cipherText: Data) throws -> ByteArray {
         // 1. Get the reference of the secret stored in the secure enclave
-        let secret = try secretReference(with: address, cipherText: cipherText)
+        let secret = try secretReference(with: id, cipherText: cipherText)
 
         // 2. Decrypt privateKey using the secret reference, and ciphertext
         var error: Unmanaged<CFError>?
         let plainTextData = SecKeyCreateDecryptedData(secret as! SecKey, Constants.algorithm, cipherText as CFData, &error) as Data?
 
         // 3. Return the Private Key using the array of bytes
-        return EthereumPrivateKey(rawBytes: plainTextData!.bytes)
+        return plainTextData!.bytes
     }
 
     private func secretReference(with reference: String, cipherText: Data) throws -> CFTypeRef? {
