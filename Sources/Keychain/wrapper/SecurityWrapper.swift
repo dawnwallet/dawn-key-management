98c27896f0140d01638cc0a66a76e96da12ea8df
Isaac <koideapps@gmail.com>
2023-02-27 11:13:13 -0800

    feat: include delete key from the keychain

--- Sources/Keychain/wrapper/SecurityWrapper.swift
+++ Sources/Keychain/wrapper/SecurityWrapper.swift
@@ -2,6 +2,7 @@ import Foundation
 import Security
 
 public protocol SecurityWrapper {
+    func SecItemDelete(_ query: CFDictionary) -> OSStatus
     func SecKeyCreateDecryptedData(_ key: SecKey, _ algorithm: SecKeyAlgorithm, _ ciphertext: CFData, _ error: UnsafeMutablePointer<Unmanaged<CFError>?>?) -> CFData?
     func SecItemAdd(_ attributes: CFDictionary, _ result: UnsafeMutablePointer<CFTypeRef?>?) -> OSStatus
     func SecKeyCopyPublicKey(_ key: SecKey) -> SecKey?
@@ -12,6 +13,10 @@ public protocol SecurityWrapper {
 
 /// Security wrapper used to easily unit test any interaction with the Keychain
 public final class SecurityWrapperImp: SecurityWrapper {
+
+    public func SecItemDelete(_ query: CFDictionary) -> OSStatus {
+        Security.SecItemDelete(query)
+    }
 
     public func SecKeyCreateDecryptedData(_ key: SecKey, _ algorithm: SecKeyAlgorithm, _ ciphertext: CFData, _ error: UnsafeMutablePointer<Unmanaged<CFError>?>?) -> CFData? {
         Security.SecKeyCreateDecryptedData(key, algorithm, ciphertext, error)
